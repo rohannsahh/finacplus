a708014c1541fe1ecf04c585b059a63d565df192
Rohan <rohannsahh@gmail.com>
2025-03-11 01:52:06 +0530

    assignment complete

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
-
-
 dotenv.config();
 
 export const ConnectDB =async()=>{
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI|| "mongodb+srv://admin:admin123@cluster0.nbjwb.mongodb.net/finacplus?retryWrites=true&w=majority&appName=Cluster0"
+    const conn = await mongoose.connect(process.env.MONGO_URI||""
 );
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
